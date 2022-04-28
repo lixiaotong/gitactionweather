7e5f71b5d558881ac896e31a7d8eeb6d16807daa
ICEEYES <276125681@qq.com>
2022-04-28 16:07:13 +0800

    update

--- weather.sh
+++ weather.sh
@@ -2,13 +2,13 @@
 
 set -eux
 
-CITY=Shanghai
-LANGUAGE="zh-CN"
-UNIT=m
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+# CITY=Shanghai
+# LANGUAGE="zh-CN"
+# UNIT=m
+# UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl \
-  -H "Accept-Language: $LANGUAGE" \
-  -H "User-Agent: $UA" \
+#   -H "Accept-Language: $LANGUAGE" \
+#   -H "User-Agent: $UA" \
   -o result.html \
   wttr.in/Shanghai
