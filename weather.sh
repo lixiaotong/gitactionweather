1a2506e6f9e5fb8b1e853e57e9caf12ec6e477d1
ICEEYES <276125681@qq.com>
2022-04-28 16:41:13 +0800

    test

--- weather.sh
+++ weather.sh
@@ -7,8 +7,10 @@ set -eux
 # UNIT=m
 # UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl \
-#   -H "Accept-Language: $LANGUAGE" \
-#   -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/Shanghai
+# curl \
+# #   -H "Accept-Language: $LANGUAGE" \
+# #   -H "User-Agent: $UA" \
+#   -o result.html \
+#   wttr.in/Shanghai
+
+wget wttr.in/Shanghai.png
